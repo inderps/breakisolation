c2bf4788ee32ebabd87bf90f740af707e36b6f3c
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-07-07 23:40:58 +0530

    draft 1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $('body').backstretch('images/bg.jpg');
+
+  function initialize() {
+    var myLatlng = new google.maps.LatLng(52.523681, 13.402309);
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: myLatlng,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hannah & Cash'
+    });
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
